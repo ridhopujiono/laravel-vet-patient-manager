bf1c9b9e15e8088d756a83e042648215f49a8bd9
Ridho Pujiono <ridhopujiono2@gmail.com>
2025-04-10 22:41:46 +0700

    feat: add Owner, Patient, and Treatment models

---
+++ app/Models/Patient.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Patient extends Model
+{
+    protected $fillable = [
+        'name',
+        'date_of_birth',
+        'type',
+        'owner_id',
+    ];
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(Owner::class);
+    }
+
+    public function treatments(): HasMany
+    {
+        return $this->hasMany(Treatment::class);
+    }
+}
